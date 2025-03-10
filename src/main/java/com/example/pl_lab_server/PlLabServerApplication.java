2bade96c20708374182589bcb6b5132934ee25ab
june <0721hjp@naver.com>
2025-03-10 16:58:59 +0900

    초기설정

---
+++ src/main/java/com/example/pl_lab_server/PlLabServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pl_lab_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlLabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlLabServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pl_lab_server/PlLabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pl_lab_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlLabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
